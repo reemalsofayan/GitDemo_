ff5cd3c85701669fa879808feaa82d8c2a715edf
FIRST_NAME LAST_NAME <reemalsofayan@gmail.com>
2021-08-01 13:27:16 +0300

    v

--- GitDemo_/src/HelloWorld.java
+++ GitDemo_/src/HelloWorld.java
@@ -1,14 +1,10 @@
 
 public class HelloWorld {
 
+	public void print2() {}
 	public void print() {}
 	
 	
-	
-	public static void main(String[] args) {
-		System.out.println("Hello world !!");
-		System.out.println("Hello Saudi Arabia!!");
 
-	}
 
 }
